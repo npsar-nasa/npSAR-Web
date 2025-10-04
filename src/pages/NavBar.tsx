f05f39569cc78d8a8978949756fe3d1f46046c71
DUDE <nisbis194@gmail.com>
2025-10-04 16:32:19 +0545

    added about section with nav bar

---
+++ src/pages/NavBar.tsx
@@ -0,0 +1,62 @@
+import { Mountain } from "lucide-react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        {/* Logo and Name */}
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Mountain color="#129900" />
+          <span className="self-center text-2xl text-green-800 font-semibold whitespace-nowrap dark:text-white">
+            Nepal's GLacier Lake
+          </span>
+        </a>
+
+        {/* Navbar Links */}
+        <div className="w-full md:block md:w-auto">
+          <ul
+            className="flex flex-col font-medium text-2xl mt-4 rounded-lg bg-gray-50 
+          md:flex-row md:space-x-8 md:mt-0 mr-6 md:border-0 md:bg-transparent 
+          dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700"
+          >
+            <li>
+              <Link
+                to="/"
+                className="block py-2 px-3 md:p-0 text-white bg-green-800 rounded-sm 
+                md:bg-transparent md:text-green-800 md:dark:text-green-500 
+                md:dark:bg-transparent"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className="block py-2 px-3 md:p-0 text-white bg-green-800 rounded-sm 
+                md:bg-transparent md:text-green-800 md:dark:text-green-500 
+                md:dark:bg-transparent"
+                aria-current="page"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/quiz"
+                className="block py-2 px-3 md:p-0 text-white bg-green-800 rounded-sm 
+                md:bg-transparent md:text-green-800 md:dark:text-green-500 
+                md:dark:bg-transparent"
+                aria-current="page"
+              >
+                Quiz
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,18 +1,22 @@
 import { Mountain, Globe, TrendingDown, AlertTriangle } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
 import { useNavigate } from "react-router-dom";
+import Navbar from "./NavBar";
+{
+  /* <button
+        className=" bg-green-800 border-green-800 m-4 hover:bg-green-950 rounded-sm  px-4 py-1 text-white"
+        onClick={() => Navigate("/")}
+      >
+        Home
+      </button> */
+}
 
 const About = () => {
   const Navigate = useNavigate();
   return (
     <div className="min-h-screen bg-background">
-      {/* <button
-        className=" bg-green-800 border-green-800 m-4 hover:bg-green-950 rounded-sm  px-4 py-1 text-white"
-        onClick={() => Navigate("/")}
-      >
-        Home
-      </button> */}
-      <div className=" text-center">
+      <Navbar></Navbar>
+      <div className=" text-center mt-10">
         <Card className="max-w-3xl mx-auto shadow-elegant bg-gradient-subtle border-primary/20">
           <CardContent className="p-8">
             <h2 className="text-2xl font-bold text-foreground mb-4 pt-3">
